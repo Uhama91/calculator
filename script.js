55cc9decf1ff778f8e30e097049799e7650b8f19
uhama <harounjean@hotmail.fr>
2025-11-27 17:37:50 +0100

    create a backspace button

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const add = (a,b) => {
-    return a+ b;
+const add = (a, b) => {
+    return a + b;
 }
 
-const subtract = (a,b) => {
+const subtract = (a, b) => {
     return a - b;
 }
 
-const multiply = (a,b) => {
+const multiply = (a, b) => {
     return a * b;
 }
 
-const divide = (a,b) => {
+const divide = (a, b) => {
     return a / b;
 }
 
 const operate = (operator, a, b) => {
-    switch(operator) {
+    switch (operator) {
         case '+':
-            return add(a,b);
+            return add(a, b);
         case '-':
-            return subtract(a,b);
+            return subtract(a, b);
         case '×':
-            return multiply(a,b);
+            return multiply(a, b);
         case '÷':
-            return divide(a,b);
+            return divide(a, b);
         default:
             return null;
     }
@@ -32,27 +32,42 @@ const operate = (operator, a, b) => {
 let displayNumber = document.getElementById('display');
 let number = document.querySelectorAll('.number');
 let shouldResetDisplay = false;
+let backspaceButton = document.querySelector('.backspace')
 
-for (let i=0; i<number.length; i++){
-    number[i].addEventListener('click', function(){
-        if(shouldResetDisplay){
+backspaceButton.addEventListener('click', ()=>{
+    let str = displayNumber.textContent;
+    displayNumber.textContent = `${str.slice(0,-1)}`;
+})
+
+for (let i = 0; i < number.length; i++) {
+    number[i].addEventListener('click', function () {
+        if (shouldResetDisplay) {
             displayNumber.textContent = '';
             firstValueOfOperation = null;
             operatorOfOperation = null;
             secondValueOfOperation = null;
             shouldResetDisplay = false;
-        }    
-        displayNumber.textContent += number[i].textContent;
+        }
+        
+        if(number[i].textContent === '.' && displayNumber.textContent === ''){
+            return displayNumber.textContent += '0.'
+        }
+
+        if(number[i].textContent === '.' && displayNumber.textContent.includes('.')){
+            return
+        } else {
+            displayNumber.textContent += number[i].textContent;
+        }
     })
 }
 
 let firstValueOfOperation = null;
 let operatorOfOperation = null;
 
-let operators= document.querySelectorAll('.operator');
+let operators = document.querySelectorAll('.operator');
 
-for (let i=0; i<operators.length; i++){
-    operators[i].addEventListener('click', function(){
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click', function () {
         firstValueOfOperation = displayNumber.textContent;
         operatorOfOperation = operators[i].textContent;
         displayNumber.textContent = '';
@@ -65,7 +80,7 @@ let equalButton = document.querySelector('.equal');
 
 equalButton.addEventListener('click', () => {
     secondValueOfOperation = displayNumber.textContent;
-    if(operatorOfOperation === '÷' && secondValueOfOperation === '0'){
+    if (operatorOfOperation === '÷' && secondValueOfOperation === '0') {
         displayNumber.textContent = 'Impossible to divide by 0';
         shouldResetDisplay = true;
     } else {
